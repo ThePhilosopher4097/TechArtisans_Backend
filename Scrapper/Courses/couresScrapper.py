b7e8281fe7d5b49ce5eff8d929a01597860d6b34
Samson Banankhu Mhango <sambanankhumhango@gmail.com>
2022-11-20 22:27:31 +0200

    small changes

--- Scrapper/Courses/couresScrapper.py
+++ Scrapper/Courses/couresScrapper.py
@@ -3,3 +3,8 @@ import requests
 from bs4 import BeautifulSoup
 from time import sleep
 import pandas as pd
+
+def get_link(url):
+    re = requests.get(url)
+    soup = bf(re.content, 'html.parser')
+    return soup
