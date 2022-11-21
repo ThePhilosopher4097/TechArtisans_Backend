370ca89212964bc1136caf531da996086f317047
Samson Banankhu Mhango <sambanankhumhango@gmail.com>
2022-11-21 13:15:02 -0500

    updated .git ignore

--- Scrapper/UserFunctions/user_func.py
+++ Scrapper/UserFunctions/user_func.py
@@ -15,15 +15,4 @@ def get_url(url):
     return soup 
 
 
-# get the beautified html file and extract job titles and job links
-def get_soup(soup):
-    
-    job_title = [] #list to store job titles
-    job_link = [] #list to store job links
-    
-    # Extracting links and job titles
-    for div in divs:
-        job_title.append(div.a.text)
-        job_link.append(base+div.a.get('href'))
-        
-    return job_title, job_link
+
