6d0349f77da3af389482d71773b7f14dc4270fa5
Samson Banankhu Mhango <sambanankhumhango@gmail.com>
2022-11-21 15:38:00 +0200

    modified internshala data

---
+++ Scrapper/UserFunctions/user_func.py
@@ -0,0 +1,29 @@
+# importing packages
+import requests
+from bs4 import BeautifulSoup
+from time import sleep
+import pandas as pd
+
+
+# get link and return a beautified(structured) html content
+def get_url(url):
+    req = requests.get(url)
+    if req.status_code != 200:
+        print(f'failed to extract {url}')
+    soup = BeautifulSoup(req.content, 'html.parser')
+    
+    return soup 
+
+
+# get the beautified html file and extract job titles and job links
+def get_soup(soup):
+    
+    job_title = [] #list to store job titles
+    job_link = [] #list to store job links
+    
+    # Extracting links and job titles
+    for div in divs:
+        job_title.append(div.a.text)
+        job_link.append(base+div.a.get('href'))
+        
+    return job_title, job_link
